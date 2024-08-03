e29934b8dca56fdcc216c435ef28c147d45ead11
Heraa Farooq <93732214+techcity1985@users.noreply.github.com>
2024-08-03 23:44:43 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,35 +1,28 @@
 from app import db
-from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
-    password_hash = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(150), nullable=False)
     role = db.Column(db.String(50), nullable=False, default='student')
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False, unique=True)
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(500), nullable=False)
-    answer = db.Column(db.String(500), nullable=False)
+    question = db.Column(db.Text, nullable=False)
+    answer = db.Column(db.Text, nullable=False)
     image_path = db.Column(db.String(100), nullable=True)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
     category = db.relationship('Category', backref=db.backref('questions', lazy=True))
 
 class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), nullable=False)
-    questions = db.relationship('Question', secondary='quiz_question', backref=db.backref('quizzes', lazy='dynamic'))
+    name = db.Column(db.String(100), nullable=False)
+    questions = db.relationship('Question', secondary='quiz_question', backref=db.backref('quizzes', lazy=True))
 
 class QuizQuestion(db.Model):
     __tablename__ = 'quiz_question'
