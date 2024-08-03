cc66689add30c7151cb9ad5a1cffe922d0e26157
Heraa Farooq <93732214+techcity1985@users.noreply.github.com>
2024-08-03 22:49:23 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,8 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FileField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SubmitField, FileField, PasswordField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class QuestionForm(FlaskForm):
     question = TextAreaField('Question', validators=[DataRequired()])
     answer = TextAreaField('Answer', validators=[DataRequired()])
-    image = FileField('Image File')
+    image_file = FileField('Image File')
+    category = SelectField('Category', coerce=int, validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class CategoryForm(FlaskForm):
+    name = StringField('Category Name', validators=[DataRequired()])
+    submit = SubmitField('Add Category')
