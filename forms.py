1ba3944e01c50c020dbc1e220e1049a9e639c38d
Heraa Farooq <93732214+techcity1985@users.noreply.github.com>
2024-08-03 23:34:38 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,26 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, FileField, PasswordField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, TextAreaField, FileField, SelectField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_wtf.file import FileAllowed
+from models import Category
 
 class QuestionForm(FlaskForm):
-    question = TextAreaField('Question', validators=[DataRequired()])
-    answer = TextAreaField('Answer', validators=[DataRequired()])
-    image_file = FileField('Image File')
-    category = SelectField('Category', coerce=int, validators=[DataRequired()])
+    question = TextAreaField('Question', validators=[DataRequired(), Length(min=10)])
+    answer = TextAreaField('Answer', validators=[DataRequired(), Length(min=1)])
+    image_file = FileField('Image File', validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+    category = SelectField('Category', coerce=int)
     submit = SubmitField('Submit')
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
-
-class CategoryForm(FlaskForm):
-    name = StringField('Category Name', validators=[DataRequired()])
-    submit = SubmitField('Add Category')
+    def __init__(self, *args, **kwargs):
+        super(QuestionForm, self).__init__(*args, **kwargs)
+        self.category.choices = [(category.id, category.name) for category in Category.query.all()]
